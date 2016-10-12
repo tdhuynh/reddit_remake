f62aa9ffb55b320b606bee4e73880cc5917170e7
tdhuynh <tommyhuynh93@gmail.com>
2016-10-12 14:19:31 -0400

    created subreddit model

--- reddit_app/models.py
+++ reddit_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Subreddit(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    creation = models.DateTimeField(auto_now_add=True)
+
+    def current_count(self):
+        pass
+
+    def today_count(self):
+        pass
+
+    def daily_average(self):
+        pass
+
+    def __str__(self):
+        return self.name
