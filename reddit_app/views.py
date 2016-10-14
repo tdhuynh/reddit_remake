83cefb8db9abd7917aa6b008c69bb485a7e597a8
tdhuynh <tommyhuynh93@gmail.com>
2016-10-14 18:23:52 -0400

    mostly done, needs css and reverse link

--- reddit_app/views.py
+++ reddit_app/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from reddit_app.models import Subreddit, Post, Comment
 
-from django.urls import reverse_lazy
+from django.urls import reverse
 
 
 def index_view(request):
@@ -34,14 +35,59 @@ class SubredditCreateView(CreateView):
 
 class SubredditUpdateView(UpdateView):
     model = Subreddit
-    success_url = "/"
     fields = ('name', 'description',)
+
+    def get_success_url(self, **kwargs):
+        return reverse('subreddit_detail_view', args=self.kwargs['pk'])
 
 
 class PostDetailView(DetailView):
     model = Post
 
+
 class PostCreateView(CreateView):
     model = Post
-    success_url = reverse_lazy('subreddit_detail_view')
     fields = ('title', 'description', 'url')
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        instance.subreddit = Subreddit.objects.get(id=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self, **kwargs):
+        return reverse('subreddit_detail_view', args=self.kwargs['pk'])
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('title', 'description', 'url')
+
+    def get_success_url(self, **kwargs):
+        return reverse('post_detail_view', args=self.kwargs['pk'])
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    fields = ('text',)
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        instance.post = Post.objects.get(id=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self, **kwargs):
+        return reverse('post_detail_view', args=self.kwargs['pk'])
+
+
+class CommentUpdateView(UpdateView):
+    model = Comment
+    success_url = "/"
+    fields = ('text',)
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = "/"
--- reddit_remake/urls.py
+++ reddit_remake/urls.py
@@ -1,14 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from reddit_app.views import SubredditView, SubredditDetailView, PostDetailView, \
-                             SubredditCreateView, SubredditUpdateView, PostCreateView
+                             SubredditCreateView, SubredditUpdateView, PostCreateView, \
+                             UserCreateView, PostUpdateView, CommentCreateView, CommentUpdateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('^', include('django.contrib.auth.urls')),
     url(r'^$', SubredditView.as_view(), name='subreddit_view'),
-    url(r'^create/$', SubredditCreateView.as_view(), name='subreddit_create_view'),
+    url(r'^create_user/$', UserCreateView.as_view(), name='user_create_view'),
+    url(r'^create_sub/$', SubredditCreateView.as_view(), name='subreddit_create_view'),
     url(r'^(?P<pk>\d+)/$', SubredditDetailView.as_view(), name='subreddit_detail_view'),
-    url(r'^(?P<pk>\d+)/update/$', SubredditUpdateView.as_view(), name='subreddit_update_view'),
-    url(r'^post/$', PostCreateView.as_view(), name='post_create_view'),
-    url(r'^post/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post_detail_view'),
+    url(r'^(?P<pk>\d+)/update_sub/$', SubredditUpdateView.as_view(), name='subreddit_update_view'),
+    url(r'^(?P<pk>\d+)/create_post/$', PostCreateView.as_view(), name='post_create_view'),
+    url(r'^(?P<pk>\d+)/post/$', PostDetailView.as_view(), name='post_detail_view'),
+    url(r'^(?P<pk>\d+)/post/update_post/$', PostUpdateView.as_view(), name='post_update_view'),
+    url(r'^post/(?P<pk>\d+)/create_comment/$', CommentCreateView.as_view(), name='comment_create_view'),
+    url(r'^(?P<pk>\d+)/update_comment/$', CommentUpdateView.as_view(), name='comment_update_view'),
 ]
